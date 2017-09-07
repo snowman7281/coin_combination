644fd0a3e07cb03454fc9cbcd89637da03cda718
Bably Kumari and snow vilay <snow@>
2017-09-07 08:58:51 -0700

    first test of taking in a user input and grabbing the value

---
+++ lib/coin_combination.rb
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+puts "please enter an amount thats under a dollar."
+amount = gets.chomp
+
+
+# public def coin_combination(cents)
+#
+# end
---
+++ spec/coin_combonation_spec.rb
@@ -0,0 +1,10 @@
+require('coin_combination')
+require('rspec')
+require('pry')
+
+
+describe("#coin_combination")do
+  it("return any value that is inputted by the user")do
+    expect((50)).to(eq(50))
+  end
+end
